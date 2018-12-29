f330142e00bede6048b109493b6202effea9a235
Тимофей <tymcrit@gmail.com>
2018-12-29 23:48:32 +0300

    Защита от двойного подключения

--- slang.hpp
+++ slang.hpp
@@ -1,3 +1,6 @@
 #include <string>
 
-std::string runSlang(const std::string & code, const std::string & input = "");
+#ifndef ___SLANG_DEFINED
+    #define ___SLANG_DEFINED
+    std::string runSlang(const std::string & code, const std::string & input = "");
+#endif
